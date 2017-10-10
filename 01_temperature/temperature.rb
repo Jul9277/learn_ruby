5985f613a6c5ab0ddcec3df45d0b482843b16907
juliette <ju.auvray@orange.fr>
2017-10-10 17:55:24 +0200

    exercices rspec

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+
+
+def ftoc (temperaturef)
+ return ((temperaturef-32)/1.8).round
+end
+
+def ctof (temperaturec)
+ return ((temperaturec *1.8)+32)
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
 #write your code here
+def echo (valeur)
+	return valeur
+end
+
+def shout (valeur)
+	return valeur.upcase!
+end
+
+def repeat (valeur, n=2)
+	return valeur+(" "+valeur)*(n-1)
+end
+
+def start_of_word (string, num)
+	string [0,num]
+end
+
+def first_word (word)
+	word.split[0]
+end
+
+def titleize (phrase)
+	little_words = ["the","over","and"]
+	phrase.capitalize.split.map { |mot|
+		if little_words.include?(mot)
+			mot
+		else
+			mot.capitalize
+		end
+	}.join(" ")
+end
